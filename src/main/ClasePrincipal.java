01dca8214b49eea5511ff15f923e07ba2a043951
T616814 <fperalta@minsait.com>
2022-01-17 17:50:28 +0100

    Segundo commit

--- src/main/ClasePrincipal.java
+++ src/main/ClasePrincipal.java
@@ -5,6 +5,7 @@ public class ClasePrincipal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Primer commit después de añadir el java
+		// Segundo commit después de añadir el java
 	}
 
 }
