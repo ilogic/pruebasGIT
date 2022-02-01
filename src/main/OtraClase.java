534be28c35483aeb3759a1916592cce9215d9080
Fer Peralta <ferperalta@gmail.com>
2022-02-01 17:25:09 +0100

    Cambio en GitHub

--- src/main/OtraClase.java
+++ src/main/OtraClase.java
@@ -4,4 +4,5 @@ public class OtraClase {
 // Añadimos algo en otra clase con checkout en la "RamaSobreWorkingParentDesdeRama1"
 // Comentario con checkout en RamaSobreRama1DesdeRama1
 // Esto es del correctivo 1
+// Cambio directamente en GITHUB para probar el pull
 }
